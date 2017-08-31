b1e3e08d670e75e317828162496819bf00ff9c50
Adon Metcalfe <adonm@fastmail.fm>
2017-08-31 16:29:17 +0800

    stock app with custom foreign models

---
+++ contribauthproxy/models.py
@@ -0,0 +1,39 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from __future__ import unicode_literals
+
+from django.db import models
+
+class ForeignAuthUser(models.Model):
+    id = models.IntegerField(primary_key=True)
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(max_length=150)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'foreign_auth_user'
+
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+@receiver(pre_save, sender=ForeignAuthUser)
+def autofieldshim(sender, instance, *args, **kwargs):
+    instance.id = ForeignAuthUser.objects.last().id + 1
+
+class UserComment(models.Model):
+    user = models.ForeignKey(ForeignAuthUser, db_constraint=False)
+    comment = models.TextField()
+
